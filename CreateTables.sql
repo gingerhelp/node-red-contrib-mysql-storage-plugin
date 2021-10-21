1cc811096decc8333b798f60797a932b3bb96eeb
GingerHelp <aellis@gingerhelp.com>
2021-10-20 20:42:55 -0400

    Removes GO statements

--- CreateTables.sql
+++ CreateTables.sql
@@ -3,13 +3,11 @@ CREATE TABLE `NodeRedCredentials` (
   `JsonData` longtext DEFAULT NULL,
   PRIMARY KEY (`CredentialID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-GO;
 CREATE TABLE `NodeRedFlows` (
   `FlowID` int(11) NOT NULL AUTO_INCREMENT,
   `JsonData` longtext NOT NULL,
   PRIMARY KEY (`FlowID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-GO;
 CREATE TABLE `NodeRedLibrary` (
   `LibraryID` int(11) NOT NULL AUTO_INCREMENT,
   `Type` varchar(10) DEFAULT NULL,
@@ -18,16 +16,13 @@ CREATE TABLE `NodeRedLibrary` (
   `JsonData` longtext DEFAULT NULL,
   PRIMARY KEY (`LibraryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-GO;
 CREATE TABLE `NodeRedSessions` (
   `SessionID` int(11) NOT NULL AUTO_INCREMENT,
   `JsonData` longtext DEFAULT NULL,
   PRIMARY KEY (`SessionID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-GO;
 CREATE TABLE `NodeRedSettings` (
   `SettingID` int(11) NOT NULL AUTO_INCREMENT,
   `JsonData` longtext DEFAULT NULL,
   PRIMARY KEY (`SettingID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-GO;
